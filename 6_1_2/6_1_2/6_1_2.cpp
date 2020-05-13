964df11fc72836e81a5b47a5604b5f4ca2d8d58c
Nikolaev Misha <63680387+nikolaevdev@users.noreply.github.com>
2020-05-13 17:16:34 +0300

    6.1.2

---
+++ 6_1_2/6_1_2/6_1_2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    setlocale(LC_ALL, "Rus");
+
+    int table[10][10];
+    int sred = 0;
+    int sum = 0;
+    int col = 0;
+
+    cout << "Массив: ";
+
+    for (int t = 0; t < 10; t++) {
+        for (int i = 0; i < 10; i++) {
+            table[t][i] = rand() % 100 + 1;
+            sum += table[t][i];
+            col++;
+            cout << table[t][i] << " ";
+        }
+
+        cout << '\n';
+
+    }
+
+    sred = sum / col;
+    cout << " Среднее арифметическое введенных чисел = " << sred << endl;
+
+    system("pause");
+    
+    return 0;
+
+}
