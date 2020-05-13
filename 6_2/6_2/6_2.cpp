4a0eac3c3b9fee1f1a9cf8091f14747610743fa2
Nikolaev Misha <63680387+nikolaevdev@users.noreply.github.com>
2020-05-13 17:54:55 +0300

    6_2

---
+++ 6_2/6_2/6_2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    setlocale(LC_ALL, "Rus");
+
+    int const n = 10;
+
+    int i, mas[n], max;
+
+    int col = 0;
+
+    cout << " Введите 10 элементов массива \n";
+
+    for (i = 0; i < 10; i++)
+    {
+        cin >> mas[i]; //Ввод данных в массив
+        col++;
+    }
+
+    max = mas[0];
+    for (i = 1; i < n; i++)
+    {
+        if (max < mas[i]) max = mas[i];
+    }
+
+    cout << " Максимальный = " << max << endl;
+
+    for (i = 0; i < n; i++)
+    {
+        if (mas[i] != max)
+            cout << "[" << i << "]" << "=" << mas[i] << " " << endl;
+    }
+
+    system("pause");
+
+    return 0;
+
+}
