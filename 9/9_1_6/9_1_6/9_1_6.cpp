757426275c05dc73e87dc755c019aeb8df4cdff1
Nikolaev Misha <63680387+nikolaevdev@users.noreply.github.com>
2020-05-22 15:33:58 +0300

    9_1_6

---
+++ 9/9_1_6/9_1_6/9_1_6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    ifstream file;
+
+    file.open("text_in.txt");
+    if (!file) return 1;
+
+    char str[80];
+    int min = 80, ns = 0, strnum = 0;
+
+    // Считывать и отображать строки в цикле, пока не eof
+    while (!file.getline(str, sizeof(str)).eof()) {
+        ns++;
+        if (strlen(str) < min) {
+            min = strlen(str);
+            strnum = ns;
+        }
+    }
+       
+    cout << "Min = " << strnum << endl;
+    file.close();
+    return 0;
+}
