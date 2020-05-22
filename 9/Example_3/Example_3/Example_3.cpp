63dd402b070a90f5abc28fa8cec73b35b6a2eaa1
Nikolaev Misha <63680387+nikolaevdev@users.noreply.github.com>
2020-05-22 13:22:45 +0300

    Example 3

---
+++ 9/Example_3/Example_3/Example_3.cpp
@@ -0,0 +1,29 @@
+﻿// Example_3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "fstream";
+#include "iostream";
+#include <iostream>;
+
+using namespace std;
+
+
+int main()
+{
+    char symbol;
+    ifstream in("text_in.txt");
+    ofstream out("text_out.txt");
+
+    while (in.peek() != EOF)
+    {
+        in.get(symbol);
+        if (symbol == 'C')
+            out << "C++";
+        else
+            out << symbol;
+    }
+
+    in.close();
+    out.close();
+    return 0;
+}
