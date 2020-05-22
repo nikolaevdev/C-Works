23e25334e70dcaf15298d6e8df29caf42beca82e
Nikolaev Misha <63680387+nikolaevdev@users.noreply.github.com>
2020-05-22 14:15:59 +0300

    Example 4

---
+++ 9/Example_4/Example_4/Example_4.cpp
@@ -0,0 +1,34 @@
+﻿// Example_4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "fstream";
+using namespace std;
+
+int main()
+{
+    ofstream out("data.dat", ios::binary);
+    double a, b, h, i;
+    cout << "a = ";
+    cin >> a;
+    cout << "b= ";
+    cin >> b;
+    cout << "h = ";
+    cin >> h;
+
+    for (i = a; i <= b; i += h)
+        out.write((char*)&i, sizeof(i));
+    out.close();
+    ifstream in("f.dat", ios::binary);
+
+    in.seekg(sizeof(double));
+
+    while (in.peek() != EOF) {
+        in.read((char*)&i, sizeof(double));
+        cout << i << ' ';
+    }
+    in.close();
+    return 0;
+
+
+}
