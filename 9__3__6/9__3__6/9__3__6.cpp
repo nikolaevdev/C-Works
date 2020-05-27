d0737cc3ca12619cc6e43c485319f83b18a8b382
Nikolaev Misha <63680387+nikolaevdev@users.noreply.github.com>
2020-05-27 14:51:38 +0300

    9_3

---
+++ 9__3__6/9__3__6/9__3__6.cpp
@@ -0,0 +1,24 @@
+﻿// 9__3__6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+
+int main()
+{
+    ifstream in("f.txt");
+
+    int i;
+    while (in >> i)
+    {
+        if (i < 0)
+            cout << setw(5) << i << endl;
+    }
+    in.close();
+    return 0;
+}
+
